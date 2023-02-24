890e540b32e1fbd97365476058c90ac5de55cdc8
Maxim Sergeev <maxsergeev39+gh@gmail.com>
2023-02-24 04:58:00 +0300

    Delete `formatter.py`

--- src/formatter.py
+++
@@ -1,9 +0,0 @@
-def format_response(prompt: str, raw_response: str):
-    if raw_response.lower().startswith(prompt.lower()):
-        return f"<b>{prompt}</b> {raw_response[len(prompt):]}"
-
-    elif raw_response[0].isupper():
-        return f"<b>{prompt}</b>\n\n{raw_response}"
-
-    else:
-        return f"<b>{prompt}</b>"
